6c724c1ebef70a8b49a29e01886f23b77ba8b347
dedieulemvodamba-cloud <dedieulemvodamba@gmail.com>
2026-09-17 17:12:38 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,3 @@
-import WelcomeIntro from "./components/WelcomeIntro";
-import AmbientSound from "./components/AmbientSound";
-
-function App() {
-  return (
-    <>
-      <WelcomeIntro />
-      <AmbientSound />
-      {/* le reste de ton application ici */}
-    </>
-  );
-}
-
-export default App;
-
 import React, { useState, useEffect, useCallback } from 'react';
 import { Book, UserAccount, SecurityAuditItem, OrderReceipt } from './types';
 import { BOOKS_COLLECTION } from './data/books';
@@ -33,6 +18,7 @@ import { SplashScreen } from './components/SplashScreen';
 import { OfflineGuideModal } from './components/OfflineGuideModal';
 import { OfflineIndicator } from './components/OfflineIndicator';
 import { ShieldAlert, KeyRound, RefreshCw, CheckCircle2 } from 'lucide-react';
+import AmbientSound from './components/AmbientSound';
 
 export default function App() {
   const [showSplash, setShowSplash] = useState(true);
@@ -191,6 +177,9 @@ export default function App() {
 
   return (
     <div className="min-h-screen flex flex-col bg-[#0c0b0f] text-[#f0ead8] selection:bg-[#c9a84c]/30 selection:text-[#f0ead8]">
+      {/* Ambient background sound */}
+      <AmbientSound />
+
       {/* Top Security Status Ribbon */}
       <SecurityBadgeBanner sessionToken={sessionToken} auditCount={auditLogs.length} />
 
